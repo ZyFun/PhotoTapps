bf1c84b8516a9e2022213f609c053c49c315e41e
Dmitriy Danilin <danilindn@icloud.com>
2020-06-15 14:26:57 +0300

    Initial Commit

---
+++ PhotoTapps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoTapps
+//
+//  Created by Дмитрий Данилин on 15.06.2020.
+//  Copyright © 2020 Дмитрий Данилин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
